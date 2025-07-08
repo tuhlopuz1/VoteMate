5d784d8d0edac60e94a5411bdae0f6873b78d68a
asdfrewqha <asdfrewqha@gmail.com>
2025-07-08 13:56:51 +0300

    pycahce clean

--- backend/core/routers_loader.py
+++ backend/core/routers_loader.py
@@ -10,7 +10,9 @@ logger = logging.getLogger(__name__)
 
 
 def include_all_routers(app: FastAPI):
-    for _, module_name, _ in pkgutil.walk_packages(backend.routes.__path__, prefix="routes."):
+    for _, module_name, _ in pkgutil.walk_packages(
+        backend.routes.__path__, prefix="backend.routes."
+    ):
         module = importlib.import_module(module_name)
 
         if hasattr(module, "router"):
