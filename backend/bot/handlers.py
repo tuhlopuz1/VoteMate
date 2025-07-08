27dad785c69ea287ccd3fc68be5206f64a6fbbe0
asdfrewqha <asdfrewqha@gmail.com>
2025-07-08 21:52:53 +0300

    redis optimize

--- backend/bot/handlers.py
+++ backend/bot/handlers.py
@@ -16,6 +16,11 @@ def generate_secure_code(length: int = 6) -> str:
 
 @router.message(Command("start"))
 async def handle_start(message: types.Message):
-    token = generate_secure_code()
-    await redis_adapter.set(f"telegram-code:{token}", message.chat.id, expire=600)
-    await message.answer(f"Ваш код: {token}")
+    existing_code = await redis_adapter.get(f"telegram-id:{message.chat.id}")
+    if existing_code:
+        await message.answer(f"Ваш код: {existing_code}")
+    else:
+        code = generate_secure_code()
+        await redis_adapter.set(f"telegram-id:{message.chat.id}", code, expire=600)
+        await redis_adapter.set(f"telegram-code:{code}", message.chat.id, expire=600)
+        await message.answer(f"Ваш код: {code}")
